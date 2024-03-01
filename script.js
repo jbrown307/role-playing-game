523c195cb8b3e215f59c511544fb40582e8275c9
Jordan Brown <jordanbrown@protonmail.com>
2024-03-01 15:58:11 -0500

    ADD functions

--- script.js
+++ script.js
@@ -25,3 +25,75 @@ const monsterName = document.querySelector("#monsterName")
 const monsterHealthText = document.querySelector("#monsterHealth")
 const healthText = document.querySelector("#healthText")
 
+// Object properties are written as key: value pairs, where key is the name of the property (or the key), and value is the value that property holds
+const locations = [
+    {
+      name: "town square",
+      "button text": ["Go to store", "Go to cave", "Fight dragon"],
+      "button functions": [goStore, goCave, fightDragon],
+      text: "You are in the town square. You see a sign that says \"Store\"." 
+    },
+
+    {
+      name: "store",
+      "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+      "button functions": [buyHealth, buyWeapon, goTown],
+      text: "You enter the store."  
+    }
+  ];
+
+// ---------- functions ----------
+
+// The innerText property controls the text that appears in an HTML element
+
+// When a player clicks your Go to store button, you want to change the buttons and text
+
+// You need to wrap the text Store in double quotes. Because your string is already wrapped in double quotes, you'll need to escape the quotes around Store. You can escape them with a backslash \.
+function goTown() {
+  update (locations[0]);
+}
+
+function goStore() {
+
+}
+
+function goCave() {
+    console.log("Going to cave.");
+}
+
+function fightDragon() {
+    console.log("Fighting dragon.");
+}
+
+function buyHealth() {
+
+}
+  
+function buyWeapon() {
+  
+}
+
+function update(location) {
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0]
+  button2.onclick = location["button functions"][1]
+  button3.onclick = location["button functions"][2]
+  text.innerText = location.text;
+}
+  
+
+
+// initialize buttons
+
+// Button elements have a special property called onclick, which you can use to determine what happens when someone clicks that button
+
+// You can access properties in JavaScript a couple of different ways. The first is with dot notation
+
+// Use dot notation to set the onclick property of your button1 to the function reference of goStore
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon; 
+
+

004b10f88324d941d63dd639c9097780b32e96e6
Jordan Brown <jordanbrown@protonmail.com>
2024-03-12 16:22:08 -0400

    ADD the rest of the functions!

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // ---------- stats vars ----------
-
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -10,96 +9,121 @@ let inventory = ["stick"];
 
 // ---------- control vars ---------- 
 
-// One method for finding specific elements in your HTML is using the querySelector() method. The querySelector() method takes a CSS selector as an argument and returns the first element that matches that selector
+/* One method for finding specific elements in your HTML is using the querySelector() method. The querySelector() method takes a CSS selector as an argument and returns the first element that matches that selector */
 
-// button1 is a variable that is not going to be reassigned. If you are not going to assign a new value to a variable, it is best practice to use the const keyword to declare it instead of the let keyword. This will tell JavaScript to throw an error if you accidentally reassign it.
-const button1 = document.querySelector("#button1");
+/* button1 is a variable that is not going to be reassigned. If you are not going to assign a new value to a variable, it is best practice to use the const keyword to declare it instead of the let keyword. This will tell JavaScript to throw an error if you accidentally reassign it */
+
+const button1 = document.querySelector('#button1');
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 
+// ---------- UI text vars ----------
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-const healthText = document.querySelector("#healthText");
 
-// Object properties are written as key: value pairs, where key is the name of the property (or the key), and value is the value that property holds
+// ---------- objects ----------
+
+/* Object properties are written as key: value pairs, where key is the name of the property (or the key), and value is the value that property holds */
+const weapons = [
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 }
+];
+const monsters = [
+  {
+    name: "slime",
+    level: 2,
+    health: 15
+  },
+  {
+    name: "fanged beast",
+    level: 8,
+    health: 60
+  },
+  {
+    name: "dragon",
+    level: 20,
+    health: 300
+  }
+]
 const locations = [
-  { name: "town square",
+  {
+    name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
     "button functions": [goStore, goCave, fightDragon],
-    text: "You are in the town square. You see a sign that says \"Store\"." },
-
-  { name: "store",
+    text: "You are in the town square. You see a sign that says \"Store\"."
+  },
+  {
+    name: "store",
     "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
     "button functions": [buyHealth, buyWeapon, goTown],
-    text: "You enter the store." },
-
-  { name: "cave",
+    text: "You enter the store."
+  },
+  {
+    name: "cave",
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
     "button functions": [fightSlime, fightBeast, goTown],
-    text: "You enter the cave. You see some monsters." },
-
-  { name:"fight",
+    text: "You enter the cave. You see some monsters."
+  },
+  {
+    name: "fight",
     "button text": ["Attack", "Dodge", "Run"],
     "button functions": [attack, dodge, goTown],
-    text: "You are fighting a monster." },
-
-    { name: "kill monster",
+    text: "You are fighting a monster."
+  },
+  {
+    name: "kill monster",
     "button text": ["Go to town square", "Go to town square", "Go to town square"],
-    "button functions": [goTown, goTown, goTown],
-    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.' },
-
-    { name: "lose",
+    "button functions": [goTown, goTown, easterEgg],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+  },
+  {
+    name: "lose",
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
-    text: "You die. &#x2620;" },
+    text: "You die. &#x2620;"
+  },
+  { 
+    name: "win", 
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"], 
+    "button functions": [restart, restart, restart], 
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;" 
+  },
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to town square?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
+  }
+];
 
-    { name: "win",
-      "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
-      "button functions": [restart, restart, restart],
-      text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;" }
-  ];
+// ---------- initialize buttons ----------
 
-const weapons = [
-  { name: 'stick', 
-    power: 5 },
-
-  { name: 'dagger', 
-    power: 30 },
-    
-  { name: 'claw hammer', 
-    power: 50 },
-    
-  { name: 'sword', 
-    power: 100 }
-  ];
-
-const monsters = [
-  { name: "slime",
-    level: 2  ,
-    health: 15 },
-  
-  { name:"fanged beast",
-    level: 8,
-    health: 60 },
-  
-  { name: "dragon",
-    level: 20,
-    health: 300 },
-  ];
+// Use dot notation to set the onclick property of your buttons 
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
 
 // ---------- functions ----------
+function update(location) {
+  monsterStats.style.display = "none";
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerHTML = location.text;
+}
 
-// The innerText property controls the text that appears in an HTML element
-
-// When a player clicks your Go to store button, you want to change the buttons and text
-
-// You need to wrap the text Store in double quotes. Because your string is already wrapped in double quotes, you'll need to escape the quotes around Store. You can escape them with a backslash \.
 function goTown() {
-  update (locations[0]);
+  update(locations[0]);
 }
 
 function goStore() {
@@ -110,38 +134,25 @@ function goCave() {
   update(locations[2]);
 }
 
-// Notice that you already have a currentWeapon variable elsewhere in your code. Since this new currentWeapon variable will be inside an if statement, it will be scoped only to that block of code
-
-// Scope is the term used to describe where a variable can be accessed. If a variable is declared inside a block of code, it is only accessible to the code inside that block. This is called block scope
-
-// The shift() method on an array removes the first element in the array and returns it
-
-// The Addition Assignment Operator (+=) adds a value to a variable
-function sellWeapon() {
-  if (inventory.length > 1) {
-    gold += 15;
-    goldText.innerText = gold;
-    let currentWeapon = inventory.shift();
-    text.innerText = "You sold a " + currentWeapon + ".";
-    text.innerText += " In your inventory you have: " + inventory;
-  } else {
-    text.innerText = "Don't sell your only weapon!";
-  }
-}
-
-// There is a shorthand way to add or subtract from a variable called compound assignment. For example, changing num = num + 5 to compound assignment would look like num += 5
+/* An if statement is used to make decisions in code. The keyword if is followed by a condition in parentheses. If the condition is true, the code inside the curly braces {} is executed. If the condition is false, the code inside the curly braces is skipped */
 function buyHealth() {
   if (gold >= 10) {
     gold -= 10;
     health += 10;
     goldText.innerText = gold;
     healthText.innerText = health;
-  } 
-  else {
-    text.innerText=("You do not have enough gold to buy health.")
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
   }
 }
-  
+
+/* Notice that you already have a currentWeapon variable elsewhere in your code. Since this new currentWeapon variable will be inside an if statement, it will be scoped only to that block of code */
+
+/* Scope is the term used to describe where a variable can be accessed. If a variable is declared inside a block of code, it is only accessible to the code inside that block. This is called block scope */
+
+/* There is a shorthand way to add or subtract from a variable called compound assignment. For example, changing num = num + 5 to compound assignment would look like num += 5 */
+
+// The Addition Assignment Operator (+=) adds a value to a variable
 function buyWeapon() {
   if (currentWeapon < weapons.length - 1) {
     if (gold >= 30) {
@@ -162,17 +173,46 @@ function buyWeapon() {
   }
 }
 
+// The shift() method on an array removes the first element in the array and returns it
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
+  }
+}
+
+function fightSlime() {
+  fighting = 0;
+  goFight();
+}
+
+function fightBeast() {
+  fighting = 1;
+  goFight();
+}
+
+function fightDragon() {
+  fighting = 2;
+  goFight();
+}
+
 function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
   monsterStats.style.display = "block";
   monsterName.innerText = monsters[fighting].name;
-  monsterHealthText.innerText = monsterHealthText;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 /* The Math object in JavaScript contains static properties and methods for mathematical constants and functions. One of those is Math.random(), which generates a random number from 0 (inclusive) to 1 (exclusive). Another is Math.floor(), which rounds a given number down to the nearest integer */
 
 /* Using these, you can generate a random number within a range. For example, this generates a random number between 1 and 5: Math.floor(Math.random() * 5) + 1;. */
+
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
@@ -193,46 +233,27 @@ function attack() {
       defeatMonster();
     }
   }
-  if (Math.random() <= .1 && inventory.length!==1) {
+  if (Math.random() <= .1 && inventory.length !== 1) {
     text.innerText += " Your " + inventory.pop() + " breaks.";
     currentWeapon--;
   }
-};
+}
 
 function getMonsterAttackValue(level) {
   const hit = (level * 5) - (Math.floor(Math.random() * xp));
   console.log(hit);
-  return hit > 0 ? hit: 0;
-};
+  return hit > 0 ? hit : 0;
+}
 
 function isMonsterHit() {
   return Math.random() > .2 || health < 20;
-};
+}
 
 function dodge() {
-  text.innerText = "You dodge the attack from the " + monsters[fighting].name; + "."; 
-};
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
 
-function fightSlime() {
-  fighting = 0;
-  goFight();
-};
-
-function fightBeast() {
-  fighting = 1;
-  goFight();
- };
- 
-function fightDragon() {
-   fighting = 2;
-   goFight();
- };
-
- function isMonsterHit() {
-  return Math.random() > .2;
- };
-
- function defeatMonster() {
+function defeatMonster() {
   gold += Math.floor(monsters[fighting].level * 6.7);
   xp += monsters[fighting].level;
   goldText.innerText = gold;
@@ -241,14 +262,12 @@ function fightDragon() {
 }
 
 function lose() {
- 
- };
+  update(locations[5]);
+}
 
- function winGame() {
-
+function winGame() {
   update(locations[6]);
-
-};
+}
 
 function restart() {
   xp = 0;
@@ -259,29 +278,54 @@ function restart() {
   goldText.innerText = gold;
   healthText.innerText = health;
   xpText.innerText = xp;
-  goTown(); 
-};
+  goTown();
+}
 
-function update(location) {
-  monsterStats.style.display = "none";
-  button1.innerText = location["button text"][0];
-  button2.innerText = location["button text"][1];
-  button3.innerText = location["button text"][2];
-  button1.onclick = location["button functions"][0]
-  button2.onclick = location["button functions"][1]
-  button3.onclick = location["button functions"][2]
-  text.innerHTML = location.text;
-};
-  
-// initialize buttons
+function easterEgg() {
+  update(locations[7]);
+}
 
-// Button elements have a special property called onclick, which you can use to determine what happens when someone clicks that button
+/* At the end of the string, before the final quote, insert the new line escape character \n. This will cause the next part you add to text.innerText to appear on a new line */
 
-// You can access properties in JavaScript a couple of different ways. The first is with dot notation
+/* A while loop accepts a condition, and will run the code in the block until the condition is no longer true */
 
-// Use dot notation to set the onclick property of your button1 to the function reference of goStore
-button1.onclick = goStore;
-button2.onclick = goCave;
-button3.onclick = fightDragon; 
+/* A for loop runs for a specific number of times. for loops are declared with three expressions separated by semicolons. for (a; b; c), where a is the initialization expression, b is the condition, and c is the final expression */
 
+/* The initialization expression is executed only once, before the loop starts, and is often used to define and set up the loop variable. Think of it like declaring a counter to use in your loop. Many for loops use i as the counter and start from 0 */
 
+/* The second statement in a for loop, the condition statement, is evaluated at the beginning of every loop iteration. The loop will continue as long as the condition evaluates to be true */
+
+/* The last statement in a for loop, the final expression, is executed at the end of each loop iteration */
+
+/* The .includes() method determines if an array contains an element and will return either true or false */
+
+function pickTwo() {
+  pick(2);
+}
+
+function pickEight() {
+  pick(8);
+}
+
+function pick(guess) {
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += "Right! You win 20 gold!";
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += "Wrong! You lose 10 health!";
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
+  }
+}
